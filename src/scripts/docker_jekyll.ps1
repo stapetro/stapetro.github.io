0b46e7dd2a35ba48a3267984d6acc773e5d58e4c
Stanislav Petrov <stanislav.petrov@gfk.com>
2019-08-23 13:02:08 +0300

    Add power shell script and change info

---
+++ src/scripts/docker_jekyll.ps1
@@ -0,0 +1,22 @@
+$env:JEKYLL_VERSION = "3.8"
+
+Function Serve
+{
+    docker run --rm -p 4000:4000 --volume="${pwd}:/srv/jekyll" --volume="${pwd}/vendor/bundle:/usr/local/bundle" `
+    -it jekyll/builder:$env:JEKYLL_VERSION bundle exec jekyll serve --config _config.yml,_config_dev.yml --watch --incremental
+}
+
+Function Build
+{
+    docker run --rm -p 4000:4000 --volume="${pwd}:/srv/jekyll" --volume="${pwd}/vendor/bundle:/usr/local/bundle" `
+    -it jekyll/builder:$env:JEKYLL_VERSION bundle exec jekyll build --config _config.yml
+}
+
+Function Install
+{
+    docker run --rm -p 4000:4000 --volume="${pwd}:/srv/jekyll" --volume="${pwd}/vendor/bundle:/usr/local/bundle" `
+    -it jekyll/builder:$env:JEKYLL_VERSION bundle install --no-deployment
+
+}
+
+Serve
