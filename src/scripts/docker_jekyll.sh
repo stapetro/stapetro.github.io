c6887642789c72de31eece9ef6c6b8521c3c97fe
Stanislav Petrov <stanislav.i.petrov@gmail.com>
2019-12-20 10:56:12 +0200

    Update bundle
    Enable drafts for dev

--- src/scripts/docker_jekyll.sh
+++ src/scripts/docker_jekyll.sh
@@ -9,7 +9,7 @@ function serve() {
     --volume="$PWD:/srv/jekyll" \
     --volume="$PWD/vendor/bundle:/usr/local/bundle" \
     -it jekyll/builder:$JEKYLL_VERSION \
-    bundle exec jekyll serve --incremental --config _config.yml,_config_dev.yml
+    bundle exec jekyll serve --incremental --drafts --config _config.yml,_config_dev.yml
 }
 
 function build() {
@@ -21,6 +21,17 @@ function build() {
     --volume="$PWD/vendor/bundle:/usr/local/bundle" \
     -it jekyll/builder:$JEKYLL_VERSION \
     bundle exec jekyll build --config _config.yml
+}
+
+function update() {
+    export JEKYLL_ENV=production
+    export JEKYLL_VERSION=3.8
+    sudo docker run --rm \
+    -p 4000:4000 \
+    --volume="$PWD:/srv/jekyll" \
+    --volume="$PWD/vendor/bundle:/usr/local/bundle" \
+    -it jekyll/builder:$JEKYLL_VERSION \
+    bundle update --bundler
 }
 
 if [ -z ${1} ]; then
